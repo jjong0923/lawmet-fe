da95c06a7c8f44cee47b6549bddcfb01384f4d3e
jongmin <bergi4025@gmail.com>
2026-05-17 23:41:12 +0900

    .

--- src/pages/AnalysisReportDetailPage.tsx
+++ src/pages/AnalysisReportDetailPage.tsx
@@ -3,8 +3,8 @@ import PrevIcon from "../assets/prev.svg?react";
 export default function AnalysisReportDetailPage() {
   return (
     <div>
-      <div className="flex items-center gap-3">
-        <PrevIcon />
+      <div className="flex items-center gap-3 text-[16px] font-bold">
+        <PrevIcon className="h-4 w-4" />
         상세 리포트
       </div>
     </div>
